f76ff3c3bf61b65d617eb8e5c94bf23aa4301fe9
David Jones <david-j-jones@hotmail.com>
2025-10-09 13:05:13 -0400

    Initial ollama extraction

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,10 @@
 import './index.css';
 import './bootstrap';
-import ComponentOllamaServer from './ComponentOllamaServer';
 import ComponentTheme from './ComponentTheme';
 import { ComponentGeneralSettings } from './components/GeneralSettings';
 
 // Export the components
 export {
-
-  ComponentOllamaServer,
   ComponentTheme,
   ComponentGeneralSettings,
 };

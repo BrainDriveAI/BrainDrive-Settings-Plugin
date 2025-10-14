7356aae9e8efb3f1b4df0e3d3e2fdbf270b7cd1e
David Jones <david-j-jones@hotmail.com>
2025-10-14 13:53:51 -0400

    Updated version

--- lifecycle_manager.py
+++ lifecycle_manager.py
@@ -102,7 +102,7 @@ class BrainDriveSettingsLifecycleManager(BaseLifecycleManager):
         self.plugin_data = {
             "name": "BrainDrive Settings",
             "description": "Basic BrainDrive Settings Plugin",
-            "version": "1.1.0",
+            "version": "1.0.7",
             "type": "frontend",
             "icon": "Dashboard",
             "category": "Utilities",
